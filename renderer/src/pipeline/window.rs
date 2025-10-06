eaa0d94aabd6825632f044175e208c224e75724f
joffyn <joffynylen@hotmail.com>
2025-10-06 19:16:14 +0200

    Functioning window

--- renderer/src/pipeline/window.rs
+++ renderer/src/pipeline/window.rs
@@ -1,44 +1,61 @@
-use winit::application::ApplicationHandler;
-use winit::event::WindowEvent;
-use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
-use winit::window::{Window, WindowId};
+//use winit::application::ApplicationHandler;
+//use winit::event::WindowEvent;
+//use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
+//use winit::window::{Window, WindowId};
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::EventLoop,
+    window::Window,
+};
 
 
-#[derive(Default)]
-pub struct Renderer
+
+//#[derive(Default)]
+//pub struct Renderer
+//{
+//    pub window: Option<Window>,
+//}
+//
+//impl ApplicationHandler for Renderer
+//{
+//    fn resumed(&mut self, event_loop: &ActiveEventLoop)
+//    {
+//        self.window = Some(event_loop.create_window(Window::default_attributes()).expect("Couldn't create default window"));
+//    }
+//
+//    fn window_event(&mut self, event_loop: &ActiveEventLoop, id: WindowId, event: WindowEvent)
+//    {
+//        match event 
+//        {
+//            WindowEvent::CloseRequested => { println!("Window closing!"); event_loop.exit(); },
+//            WindowEvent::RedrawRequested => { self.window.as_ref().unwrap().request_redraw(); },
+//            _ => (),
+//        }
+//    }
+//}
+
+
+ 
+pub fn create_default_window()
 {
-    pub window: Option<Window>,
-}
+    let event_loop = EventLoop::new();
+    let window = Window::new(&event_loop).expect("Couldn't create window!");
 
-impl ApplicationHandler for Renderer
-{
-    fn resumed(&mut self, event_loop: &ActiveEventLoop)
-    {
-        self.window = Some(event_loop.create_window(Window::default_attributes()).expect("Couldn't create default window"));
-    }
-
-    fn window_event(&mut self, event_loop: &ActiveEventLoop, id: WindowId, event: WindowEvent)
+    event_loop.run(move |event, _, control_flow| 
     {
         match event 
         {
-            WindowEvent::CloseRequested => { println!("Window closing!"); event_loop.exit(); },
-            WindowEvent::RedrawRequested => { self.window.as_ref().unwrap().request_redraw(); },
+            Event::WindowEvent 
+                { 
+                event: WindowEvent::CloseRequested, .. } => 
+            { 
+                println!("Window closing!");
+                control_flow.set_exit();
+            },
+            Event::MainEventsCleared => { window.request_redraw();},
             _ => (),
         }
-    }
-}
-
- 
-pub fn create_default_window() -> Renderer
-{
-    let event_loop = EventLoop::new().expect("Couldn't create eventloop");
-    let mut renderer = Renderer::default();
-    event_loop.set_control_flow(ControlFlow::Poll);
-    event_loop.run_app(&mut renderer);
-    renderer
-
-    //let def_attr = Window::default_attributes().with_title("Cool window");
-    //event_loop.create_window(def_attr).expect("Couldn't create default window");
+    });
 }
 
 
