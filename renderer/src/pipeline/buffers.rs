93c94ed8499d21842730edd689f3e6552f6ab3d9
joffyn <joffynylen@hotmail.com>
2025-10-07 17:34:15 +0200

    Added begin function for recording commandbuffer

--- renderer/src/pipeline/buffers.rs
+++ renderer/src/pipeline/buffers.rs
@@ -3,13 +3,15 @@ use vulkano::memory::allocator::{AllocationCreateInfo, MemoryTypeFilter};
 use std::sync::Arc;
 use vulkano::memory::allocator::StandardMemoryAllocator;
 use vulkano::buffer::Subbuffer;
+use vulkano::command_buffer::{
+    AutoCommandBufferBuilder, CommandBufferUsage, PrimaryCommandBufferAbstract,
+    SubpassContents, PrimaryAutoCommandBuffer
+};
+use vulkano::command_buffer::allocator::{StandardCommandBufferAllocator, StandardCommandBufferAllocatorCreateInfo};
+use vulkano::device::{Device, Queue};
 
 use crate::pipeline::vertex::TestVertex;
 
-struct VertexBuffer
-{
-
-}
 
 pub fn create_vertex_buffer(malloc: Arc<StandardMemoryAllocator>, verts: Vec<TestVertex>) -> Subbuffer<[TestVertex]>
 {
@@ -21,4 +23,19 @@ pub fn create_vertex_buffer(malloc: Arc<StandardMemoryAllocator>, verts: Vec<Tes
     verts).expect("Couldn't create vertex buffer")
 }
 
+pub fn begin_commandbuffer_recording(queue: Arc<Queue>, device: Arc<Device>) -> AutoCommandBufferBuilder<PrimaryAutoCommandBuffer>
+{
+
+    let cba = StandardCommandBufferAllocator::new(
+        device.clone(),
+        StandardCommandBufferAllocatorCreateInfo::default(),
+    );
+
+    AutoCommandBufferBuilder::primary(
+        &cba,
+        queue.clone().queue_family_index(),
+        CommandBufferUsage::OneTimeSubmit,
+    )
+    .expect("Couldn't create commandbuffer builder")
+}
 
